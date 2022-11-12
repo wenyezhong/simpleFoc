e61fd2faf6e16f1fea6e4760cae8921e851b3c90
wenyezhong <251654680@qq.com>
2022-11-12 17:03:14 +0800

    removed MDK-ARM

--- Board/MDK-ARM/simpleFoc/simplefocdebug.d
+++
@@ -1,4 +0,0 @@
-simplefoc\simplefocdebug.o: ..\..\communication\SimpleFOCDebug.cpp
-simplefoc\simplefocdebug.o: ..\..\communication\SimpleFOCDebug.h
-simplefoc\simplefocdebug.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-simplefoc\simplefocdebug.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
